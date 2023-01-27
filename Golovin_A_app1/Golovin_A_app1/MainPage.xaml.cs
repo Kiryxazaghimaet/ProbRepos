582219b1d592c731dd033b7ade6e78cc9ddbbf80
DESKTOP-B2PN12T\SP4-IVC <shsdfh>
2023-01-27 13:01:44 +0300

    Добавили проект в репозиторий

---
+++ Golovin_A_app1/Golovin_A_app1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Golovin_A_app1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+            button1.Text = "Нажато";
+        }
+    }
+}
---
+++ Golovin_A_app1/Golovin_A_app1/StartPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Golovin_A_app1
+{
+     class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Label header = new Label()
+            {
+                Text = "Привет из Xamarin F!orms"
+            };
+            this.Content = header;
+        }
+    }
+}
